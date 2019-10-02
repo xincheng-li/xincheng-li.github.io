983035fbacbec969a4a983f272faf442c70b4ddc
Xincheng Li <xincheng.li@qq.com>
2019-10-02 08:51:40 +0700

    upgrade

--- js/date.js
+++
@@ -1,6 +0,0 @@
-function wol(){
-var mydateInput = document.getElementById("mydate");
-var date = new Date();
-var dateString = date.getFullYear()+"/"+(date.getMonth()+1)+"/"+date.getDate();
-mydateInput.value=dateString;
-}
